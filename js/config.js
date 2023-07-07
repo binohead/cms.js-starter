c6bec6f8b26a1192e1c79f3190737779fea99f1f
binohead <68013604+binohead@users.noreply.github.com>
2023-07-07 12:54:38 -0300

    Update config.js
    first attempt at setup

--- js/config.js
+++ js/config.js
@@ -11,9 +11,9 @@ var config = {
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from.
   github: {
-    username: 'yourusername',
-    repo: 'yourrepo',
-    branch: 'gh-pages',
+    username: 'binohead',
+    repo: 'cms.js-starter',
+    branch: 'main',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',

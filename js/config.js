dca81f08b4689d9897836c24a55b61a61847d2f4
binohead <68013604+binohead@users.noreply.github.com>
2023-07-07 12:55:41 -0300

    Update config.js
    oops, changed branch back again

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var config = {
   github: {
     username: 'binohead',
     repo: 'cms.js-starter',
-    branch: 'main',
+    branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',
